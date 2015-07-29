d0775b6cc6c299ec73510fefdf86f7103f1bae6d
Pradeep <pradeepkn.pradi@gmail.com>
2015-07-29 23:44:15 +0530

    Offline shopping catalogue project file is added.

---
+++ Offline_ShoppingTests/Offline_ShoppingTests.m
@@ -0,0 +1,40 @@
+//
+//  Offline_ShoppingTests.m
+//  Offline_ShoppingTests
+//
+//  Created by Pradeep Narendra on 7/27/15.
+//  Copyright (c) 2015 Pradeep Narendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Offline_ShoppingTests : XCTestCase
+
+@end
+
+@implementation Offline_ShoppingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
